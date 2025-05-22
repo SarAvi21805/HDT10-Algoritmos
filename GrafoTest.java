d83defea428cedd67d1ebe05d1ff9e139f67862f
unknown <alejandraviles2005@gmail.com>
2025-05-22 12:16:10 -0600

    Se trabajó en la Junit para agregar vertices e inicializar el grafo.

---
+++ GrafoTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GrafoTest {
+    private Grafo grafo;
+
+    @Before
+    public void setUp() {
+        grafo = new Grafo(5);
+        // A: 0, B: 1, C: 2, D: 3, E: 4
+        grafo.agregarVertice(0, 1, 3); // A -> B
+        grafo.agregarVertice(1, 2, 1); // B -> C
+        grafo.agregarVertice(0, 3, 7); // A -> D
+        grafo.agregarVertice(1, 4, 8); // B -> E
+        grafo.agregarVertice(3, 4, 3); // D -> E
+        grafo.agregarVertice(2, 3, 2); // C -> D
+        grafo.agregarVertice(4, 0, 4); // E -> A
+    }
+
+    @Test
+    public void testAgregarVertice() {
+        grafo.agregarVertice(0, 2, 5); // A -> C
+        int[][] matriz = grafo.getMatrizAdyacencia();
+        assertEquals(5, matriz[0][2]);
+    }
+}
